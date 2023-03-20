88e1b600e49949251620f6661384333e08c321f9
jiaqiluan99 <78166596+jiaqiluan99@users.noreply.github.com>
2023-03-20 15:18:10 -0400

    try

--- Services/Recruiting/ApplicationCore/Models/JobRequestModel.cs
+++ Services/Recruiting/ApplicationCore/Models/JobRequestModel.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace ApplicationCore.Models;
 
 public class JobRequestModel
 {
     public int Id { get; set; }
+
+    [StringLength(256)]
     public string Title { get; set; }
     public string Description { get; set; }
     public DateTime StartDate { get; set; }
