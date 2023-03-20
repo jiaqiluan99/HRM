8f5271973f36fea5fe930466b538bb575898e163
jiaqiluan99 <78166596+jiaqiluan99@users.noreply.github.com>
2023-03-19 22:53:33 -0400

    Add files via upload

---
+++ Services/Recruiting/RecruitingWeb/Controllers/JobsController.cs
@@ -0,0 +1,33 @@
+﻿using ApplicationCore.Contracts.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RecruitingWeb.Controllers
+{
+    public class JobsController : Controller
+    {
+        private readonly IJobService _jobService;
+        public JobsController(IJobService jobService)
+        {
+            _jobService = jobService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var jobs = await _jobService.GetAllJobs();
+            return View(jobs);
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            // get job by Id
+            var job = await _jobService.GetJobById(id);
+            return View(job);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
+
